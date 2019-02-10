c421f784caeece0d7e4fcf5950abe2cc1bb4517e
Sealdolphin <sealdolphin@github.com>
2019-02-10 17:57:13 +0100

    StatisticsWindow starting

---
+++ src/view/StatisticsWindow.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.*;
+
+public class StatisticsWindow extends JFrame {
+
+    public StatisticsWindow(){
+        setTitle("Statisztikák");
+
+        add(new JLabel("STAT: 3"));
+    }
+
+}
