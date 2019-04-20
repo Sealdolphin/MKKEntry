3a4b851c4f0b10f946c9ca3868e60e40de8a6026
Sealdolphin <sealdolphin@sch.bme.hu>
2019-04-20 12:47:04 +0200

    Created Barcode and BarcodeWizard classes
    Barcode will be another modifier class. It represents a barcode which can be used for other classes (such as Discounts)

--- src/control/modifier/Barcode.java
+++ src/control/modifier/Barcode.java
@@ -1,5 +1,6 @@
 package control.modifier;
 
+
 import view.ImagePanel;
 
 import javax.swing.*;
@@ -9,7 +10,6 @@ import java.util.List;
 public class Barcode implements Modifier{
     private String name;
     private String picturePath;
-    private String toolTip;
     private String description;
 
 
@@ -18,8 +18,11 @@ public class Barcode implements Modifier{
     }
 
     public JPanel getBarcodePanel() {
-
-        return null;
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel,BoxLayout.PAGE_AXIS));
+        panel.add(new ImagePanel(picturePath));
+        panel.add(new JLabel(description));
+        return panel;
     }
 
     @Override
@@ -29,7 +32,7 @@ public class Barcode implements Modifier{
 
     @Override
     public ModifierDialog getModifierWizard(Window parent) {
-        return new BarcodeCreator(parent);
+        return new BarcodeWizard(parent);
     }
 
     public static class BarcodeReaderListener extends ModifierWizardEditor<Barcode> {
@@ -49,30 +52,44 @@ public class Barcode implements Modifier{
         }
     }
 
-    private class BarcodeCreator extends ModifierDialog {
+    private class BarcodeWizard extends ModifierDialog {
 
         private ImagePanel panelImg = new ImagePanel(picturePath);
         private JTextField tfName = new JTextField(name);
         private JTextField tfTooltip = new JTextField(description);
         private JButton btnSetPic = new JButton("Kép módosítása");
 
-        BarcodeCreator(Window parent) {
+        BarcodeWizard(Window parent) {
+            //Set super
             super(parent, "Vonalkód módosítása");
+            btnSave.addActionListener(e->saveBarcode());
+
+            //Setup buttons
+            btnSetPic.addActionListener(e -> selectPicture(panelImg));
+
             body.setLayout(new GridBagLayout());
-
+            //Barcode image
             body.add(panelImg,setConstraints(0,0,2,1));
-
             body.add(btnSetPic,setConstraints(0,1,2,1));
-
+            //Barcode name
             body.add(new JLabel("Név"),setConstraints(0,2,1,1));
             body.add(tfName,setConstraints(1,2,1,1));
-
+            //Barcode description
             body.add(new JLabel("Leírás"),setConstraints(0,3,1,1));
             body.add(tfTooltip,setConstraints(1,3,1,1));
 
+            //Protocol!!!
             finishDialog(parent);
-
         }
+
+        private void saveBarcode() {
+            picturePath = panelImg.getPath();
+            name = tfName.getText();
+            description = tfTooltip.getText();
+            dispose();
+        }
+
+
     }
 
 }
